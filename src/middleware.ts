72a89956a14b514ddd0520904e27c950a1a3fa79
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2024-04-18 11:51:39 +0900

    Fix import and type issues in ProductTable component

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,11 @@ import type { ScreenPermissions } from "@/types"
 import { withAuth } from "next-auth/middleware"
 
 import { redirects } from "@/lib/constants"
-import { logMessage } from "@/lib/logger"
 import { checkScreenPermissions } from "@/lib/utils"
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
-    logMessage({ message: "custom middleware", object: req.nextauth.token })
     const currentPath = req.nextUrl.pathname
     const url = new URL(req.nextUrl.origin)
 
